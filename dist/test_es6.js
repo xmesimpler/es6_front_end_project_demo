74011f3610ead19be3ef99111a11108f349907a8
zeren <xmesimpler@gmail.com>
2018-04-18 19:14:55 +0800

    basic front end project arch with git, gulp, babel, eslint.

---
+++ dist/test_es6.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var intArray = [1, 2, 3, 4];
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = intArray[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var i = _step.value;
+
+    console.log(i);
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
---
+++ es6/test_es6.js
@@ -0,0 +1,4 @@
+const intArray = [1,2,3,4];
+for (let i of intArray) {
+    console.log(i);
+}
